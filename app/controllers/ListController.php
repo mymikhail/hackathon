3d8e8d3bfe1e4bb7cdc1b52e53fd4470ba59ccbb
Aleksandr Popov <apopov@imhoclub.ru>
2014-04-17 19:31:11 +0400

    поиск вроде сделан

--- app/controllers/ListController.php
+++ app/controllers/ListController.php
@@ -13,14 +13,29 @@ class ListController extends Controller
         $query= isset($_GET['query']) ? $_GET['query']: '';
         $page = isset($_GET['page']) ? intval($_GET['page']) : 0;
 
-        if (!$query) {
-            $result = $this->getFromCbView([
+        $title = isset($_GET['title']) ? $_GET['title']: '';
+        $genres = isset($_GET['genres']) ? $_GET['genres']: '';
+        $year = isset($_GET['year']) ? $_GET['year']: '';;
+        $actors = isset($_GET['actors']) ? $_GET['actors']: '';;
+        $directors = isset($_GET['directors']) ? $_GET['directors']: '';;
+        $producers = isset($_GET['producers']) ? $_GET['producers']: '';;
+        $advanced_search = ($title || $genres || $year || $actors || $directors || $producers);
+
+
+        if ($query) {
+            $result = $this->getFromElastic($query, [
+                'descending'    => true,
+                'limit'         => self::LIMIT_ON_PAGE,
+                'skip'          => self::LIMIT_ON_PAGE * $page,
+            ]);
+        } elseif ($advanced_search) {
+            $result = $this->getFromElasticAdvanced($title, $genres, $year, $actors, $directors, $producers, [
                 'descending'    => true,
                 'limit'         => self::LIMIT_ON_PAGE,
                 'skip'          => self::LIMIT_ON_PAGE * $page,
             ]);
         } else {
-            $result = $this->getFromElastic($query, [
+            $result = $this->getFromCbView([
                 'descending'    => true,
                 'limit'         => self::LIMIT_ON_PAGE,
                 'skip'          => self::LIMIT_ON_PAGE * $page,
@@ -66,6 +81,107 @@ class ListController extends Controller
             'index' => 'films',
             'type' => 'couchbaseDocument',
             'body' => json_decode($search),
+        ];
+
+        $searchResult = $client->search($searchParams);
+
+        $doc_ids = array_map(function($result) {
+                return $result['_id'];
+            }, $searchResult['hits']['hits']
+        );
+
+        $couchbase = new Couchbase('10.20.10.149:8091', '', '', 'films');
+        $docs = $couchbase->getMulti($doc_ids);
+
+        $result = ['total_rows' => $searchResult['hits']['total'], 'rows' => []];
+        foreach ($docs as $id => $doc) {
+            $result['rows'][] = [
+                'id' => $id,
+                'key' => $id,
+                'value' => json_decode($doc)
+            ];
+        }
+        return $result;
+    }
+
+    private function getFromElasticAdvanced($title, $genres, $year, $actors, $directors, $producers, $params)
+    {
+        $client = new Elasticsearch\Client([
+                'hosts' => ['10.20.10.149:9200',]
+            ]);
+
+        $must = [];
+        $must[] = [
+            'term' => [
+                'couchbaseDocument.doc.type' => 'film',
+            ],
+        ];
+
+        if ($title) {
+            $must[] = [
+                'fuzzy' => [
+                    'couchbaseDocument.doc.title' => [
+                        'value' => $title,
+                    ],
+                ],
+            ];
+        }
+        if ($genres) {
+            $must[] = [
+                'fuzzy' => [
+                    'couchbaseDocument.doc.genres' => [
+                        'value' => $genres,
+                    ],
+                ],
+            ];
+        }
+        if ($year) {
+            $must[] = [
+                'text' => [
+                    'couchbaseDocument.doc.year' => $year,
+                ],
+            ];
+        }
+        if ($actors) {
+            $must[] = [
+                'fuzzy' => [
+                    'couchbaseDocument.doc.actors.name' => [
+                        'value' => $actors,
+                    ],
+                ],
+            ];
+        }
+        if ($directors) {
+            $must[] = [
+                'fuzzy' => [
+                    'couchbaseDocument.doc.directors.name' => [
+                        'value' => $directors,
+                    ],
+                ],
+            ];
+        }
+        if ($producers) {
+            $must[] = [
+                'fuzzy' => [
+                    'couchbaseDocument.doc.producers.name' => [
+                        'value' => $producers,
+                    ],
+                ],
+            ];
+        }
+
+        $searchParams = [
+            'index' => 'films',
+            'type' => 'couchbaseDocument',
+            'body' => [
+                'query' => [
+                    'bool' => [
+                        'must' => $must,
+                    ],
+                ],
+                'from' => 0,
+                'size' => 10,
+            ],
         ];
 
         $searchResult = $client->search($searchParams);
